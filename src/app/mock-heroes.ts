b765f5766051136b91c7bc2a1c759bebcf1351d1
madrym <michael_madry@hotmail.com>
2019-11-20 14:38:20 +1100

    5 files modified

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice', },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice', power: ''},
+  { id: 12, name: 'Narco', power: ''},
+  { id: 13, name: 'Bombasto', power: ''},
+  { id: 14, name: 'Celeritas', power: ''},
+  { id: 15, name: 'Magneta', power: '' },
+  { id: 16, name: 'RubberMan', power: '' },
+  { id: 17, name: 'Dynama', power: '' },
+  { id: 18, name: 'Dr IQ', power: '' },
+  { id: 19, name: 'Magma', power: ''},
+  { id: 20, name: 'Tornado', power: ''}
 ];
